e9d7f7de5123ba8142d3fc6b99c3efe2da0cdfce
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2024-05-12 21:34:28 +0200

    Temperature measure in thread

--- app.py
+++ app.py
@@ -6,31 +6,45 @@ from w1thermsensor import W1ThermSensor
 
 import json
 import datetime
+import time
+import threading
 
 with open('configs.json') as file:
     configs = json.load(file)
 
 app = Flask(__name__)
 
-w1_sensor = None
+temperature = None
+temperature_loaded = None
 
 
-def load_sensors():
-    global w1_sensor
-
-    try:
-        w1_sensor.get_temperature()
-    except Exception:
+def measure_temperature():
+    global temperature, temperature_loaded
+    sensor_loaded = False
+    sensor = None
+    while not sensor_loaded:
         try:
-            w1_sensor = W1ThermSensor()
+            sensor = W1ThermSensor()
         except Exception:
-            pass
+            time.sleep(1)
+        else:
+            sensor_loaded = True
+    while True:
+        try:
+            temperature = sensor.get_temperature()
+            temperature_loaded = datetime.datetime.now()
+        except Exception:
+            if temperature_loaded is not None and \
+               (datetime.datetime.now() - temperature_loaded).seconds > 60:
+                temperature = None
+                temperature_loaded = None
+        time.sleep(1)
 
 
-try:
-    load_sensors()
-except Exception:
-    pass
+@app.before_first_request
+def start_thread():
+    temperature_measure_thread = threading.Thread(target=measure_temperature)
+    temperature_measure_thread.start()
 
 
 @app.route('/favicon.ico')
@@ -55,30 +69,25 @@ def index():
 
 @app.route('/api/temperature')
 def get_temperature():
-    try:
-        load_sensors()
-    except Exception:
-        pass
+    global temperature, temperature_loaded
 
-    try:
-        w1_temperature = w1_sensor.get_temperature()
-    except Exception:
-        temperature = None
-        status = 'error'
+    if temperature is not None:
+        tz_name = str(tzlocal.get_localzone())
+        tz = pytz.timezone(tz_name)
+        dt = tz.localize(temperature_loaded)
+        iso = dt.isoformat()
+
+        return {
+            "status": "ok",
+            "date": iso,
+            "temperature": temperature
+        }
     else:
-        temperature = w1_temperature
-        status = 'ok'
-
-    tz_name = str(tzlocal.get_localzone())
-    tz = pytz.timezone(tz_name)
-    dt = tz.localize(datetime.datetime.now())
-    iso = dt.isoformat()
-
-    return {
-        "status": status,
-        "date": iso,
-        "temperature": temperature
-    }
+        return {
+            "status": "error",
+            "date": None,
+            "temperature": None
+        }
 
 
 if __name__ == '__main__':
