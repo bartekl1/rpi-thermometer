af0fd9a2da95e54f78441e212d23c37616135134
bartekl1 <123882445+bartekl1@users.noreply.github.com>
2024-05-12 21:58:37 +0200

    Add humidity measure to website

--- static/js/index.js
+++ static/js/index.js
@@ -10,6 +10,27 @@ function setTemperature() {
     })
 }
 
+function setHumidity() {
+    fetch("/api/humidity").then((response) => { return response.json() }).then((json) => {
+        if (json.status === "ok" && json.humidity !== null) {
+            document.querySelector("#humidity").innerHTML = `${json.humidity.toFixed(0)}%`;
+        } else {
+            document.querySelector("#humidity").innerHTML = "--";
+        }
+    }).catch(() => {
+        document.querySelector("#humidity").innerHTML = "--";
+    })
+}
+
 setInterval(setTemperature, 30_000);
+setInterval(setHumidity, 30_000);
 
 setTemperature();
+setHumidity();
+
+console.log(
+    "%cRaspberry Pi Thermometer\n\n%cby @bartekl1\nv. 1.0\n\n%chttps://github.com/bartekl1/rpi-thermometer",
+    "font-size: 36px; font-weight: 600;",
+    "font-size: 16px;",
+    "font-size: 14px;"
+);
